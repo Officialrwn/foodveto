e55a45aa0384f2e33703135fae3b67817ddaec75
Leo MPB <vanleotran@gmail.com>
2023-03-24 07:14:01 +0200

    installed dependencies and removed example project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { createWorker } from 'tesseract.js'
+import { useEffect, useState } from 'react'
 import './App.css';
 
-function App() {
+const test = async (setMenu) => {
+	const worker = await createWorker({
+		logger: m => console.log("Reading")
+	});
+	
+	await worker.loadLanguage('eng');
+	await worker.initialize('eng');
+	const { data: { text } } = await worker.recognize('https://lh6.googleusercontent.com/ISNXu34rPPkzrle9k2NxdpvpjVAWAvKF0QdLr8PfnOBMBoohsmBFdb26zLYoUSk0-emjVL0wE1mXD24xeZk5pPUVVU24yHrZ_D3lTmeDddAEtICufKrTTnb5tFVSVeAxcQ=w1280');
+	const menu = text.split("\n");
+	// console.log(menu);
+	setMenu(menu);
+	await worker.terminate();
+}
+
+const App = () => {
+	const [useMenu, setMenu] = useState([]);
+	useEffect(() => {
+		test(setMenu);
+		console.log("MENU: ", useMenu);
+	}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			{ useMenu.map((item, index) => {
+				return (
+					<p key={index}>{item}</p>
+				);
+			})}
     </div>
   );
 }
